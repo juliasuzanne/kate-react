fa9ce3e2c9c47a20782939c4bb52c37cecf8d4ef
Julia Grimes <julia.s.grimes@gmail.com>
2023-07-01 01:07:54 -0400

    Adding count to results after filter, using useEffect maybe should change in the future

--- src/Submit.jsx
+++ src/Submit.jsx
@@ -4,8 +4,9 @@ import { ModalQuestion } from "./ModalQuestion";
 export function Submit(props) {
   const [searchFilter, setSearchFilter] = useState("  ");
   const [savedValue, setSavedValue] = useState("");
-  let [count, setCount] = useState(0);
-  let [text, setText] = useState("");
+  let [count, setCount] = useState(1);
+  let [text, setText] = useState("   ");
+  let [checked, setChecked] = useState(false);
 
   const [isQuestionModalVisible, setIsQuestionModalVisible] = useState(false);
 
@@ -26,8 +27,42 @@ export function Submit(props) {
       setSavedValue("");
     } else {
       setSearchFilter(savedValue.toLowerCase());
+      countSearchFilter();
+      setChecked(true);
     }
   };
+
+  const checkCount = () => {
+    if (count == 0 && checked == true) {
+      setText("No Results : (");
+    } else {
+      setText("   ");
+    }
+  };
+
+  const countSearchFilter = () => {
+    {
+      let newVar = props.drawings
+        .filter((drawings) => drawings.tags.toLowerCase().includes(searchFilter))
+        .map((drawing) => (
+          <div key={drawing.id} id="drawing">
+            <img
+              onClick={() => props.onShowDrawing(drawing)}
+              height="300px"
+              className="showingdrawing"
+              src={drawing.url}
+              alt=""
+            />
+            {/* <button onClick={() => props.onReturndrawing(drawing)}> Return drawing, no refunds!</button> */}
+          </div>
+        )).length;
+
+      setCount(newVar);
+    }
+  };
+
+  useEffect(countSearchFilter);
+  useEffect(checkCount);
 
   return (
     <div>
@@ -51,7 +86,8 @@ export function Submit(props) {
 
       <ModalQuestion show={isQuestionModalVisible} onClose={handleCloseQuestion}></ModalQuestion>
 
-      <h2 className="heading">{text}</h2>
+      <h2 className="heading-noresults">{text}</h2>
+
       <div id="drawings-index">
         {props.drawings
           .filter((drawings) => drawings.tags.toLowerCase().includes(searchFilter))
